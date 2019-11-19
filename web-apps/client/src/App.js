8252849222a00740b38ec2745a1f1ba99ef398e6
LEE SUK JAE <lsj7000@gmail.com>
2019-11-19 15:17:01 +0900

    refactor: apply prettier to CRA
    - CRA 로 생성된 파일들에 prettier 규칙을 적용하였습니다.
    - serviceworker와 build/ 는 규칙에서 제외하였습니다.
      - 이 부분들은 주어지는 코드를 그대로 활용합니다

--- web-apps/client/src/App.js
+++ web-apps/client/src/App.js
@@ -4,17 +4,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
