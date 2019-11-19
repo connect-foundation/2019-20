55c9eca248134785238d964202c0a74544d2e027
LEE SUK JAE <lsj7000@gmail.com>
2019-11-19 19:01:17 +0900

    feat: setup redux-logger for redux
    - redux를 사용하면서, action이나 값의 흐름들을 기록하여 보기 편하게 바꿔주는 라이브러리
    - redux에서 제공하는 createStore는, 인자를 2개만 받을 수 있다.
      - 별도의 middleware를 추가하기 위해서는 3번째 인자가 아닌 compose를 사용해야 한다.
      - compose로 두개의 middleware를 감싸면 되지만, 우리가 사용중인 composeWithDevTools에 이미 그 기능이 포함되어 있다.
      - 따라서, composeWithDevTools안에 logger를 apply 해주는 형태로 구현하였다.
    
    참고: https://github.com/jhen0409/react-native-debugger/issues/280\#issuecomment-438229613

--- web-apps/client/src/index.js
+++ web-apps/client/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './modules';
+import {createLogger} from 'redux-logger';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const logger = createLogger();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger)),
+);
 
 ReactDOM.render(
   <Provider store={store}>
