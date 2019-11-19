e264fe3353210da94e737dd54d013ad07f2f6e95
Johnie-Yeo <ducl7023@gmail.com>
2019-11-19 18:36:21 +0900

    feat: fix precommit to add test
    - husky를 이용하여 precommit에서 테스팅을 수행하는 기능을 추가하였습니다.

--- apis/template/app.js
+++ apis/template/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
---
+++ apis/template/tests/index.test.js
@@ -0,0 +1,3 @@
+it('Test to test', () => {
+  expect(1).toBe(1);
+});
