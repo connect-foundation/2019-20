3298bcb315bb83635c746c44af37a825cf373d5e
LEE SUK JAE <lsj7000@gmail.com>
2019-11-19 16:30:16 +0900

    feat: setup redux store
    - redux store를 생성하고, 설정하였습니다.

--- web-apps/client/src/index.js
+++ web-apps/client/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './modules';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+
 serviceWorker.unregister();
