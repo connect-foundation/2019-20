588b7bf076374376b84b443caba314a2e81be5c1
LEE SUK JAE <lsj7000@gmail.com>
2019-11-19 16:35:56 +0900

    feat: setup redux dev tools extension
    - chrome에서 redux dev 도구를 사용하기 위한 간편한 설정입니다.
    - 이 모듈은 dev dependencies 가 아닌, dependencies로 추가되었다. 그에 대한 근거는
    
    1. 공식 문서에서 dev가 아닌 dependencies로 추가 되어있다. (https://github.com/zalmoxisus/redux-devtools-extension\#13-use-redux-devtools-extension-package-from-npm)
    2. stackoverflow 질문에서 (https://stackoverflow.com/q/55269589) sourcefile에 이 라이브러리를 import하게 되는데,
      - production에서는 이 라이브러리가 설치되어 있지 않으므로 오류가 날 수 있다.
      - 이러한 경우는, 위 라이브러리가 설치되어있는지 방어적으로 체크하는 로직을 추가할 수 있으나, 현재는 이러한 방법으로 선택하였다.

--- web-apps/client/src/index.js
+++ web-apps/client/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './modules';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
