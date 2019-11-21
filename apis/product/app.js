1b8b7189124bfed15d405765d485a643453db47f
Johnie-Yeo <ducl7023@gmail.com>
2019-11-21 13:15:29 +0900

    fix: fix babel setting
    - babel에서 async await에 대한 트랜스 파일링 오류를 수정하였습니다.

--- apis/product/app.js
+++ apis/product/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv';
-
+import 'core-js';
 import db from './db';
 
 dotenv.config();
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
