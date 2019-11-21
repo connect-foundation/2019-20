d6359e89c26cb62c9e312076bae13e311f938bdc
Johnie-Yeo <ducl7023@gmail.com>
2019-11-21 13:52:24 +0900

    fix: fix babel setting
    - babel 세팅에 대한 오류 사항이 있어 이를 수정합니다.

--- apis/template/app.js
+++ apis/template/app.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-
+import 'core-js';
 const indexRouter = require('./routes/index');
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
