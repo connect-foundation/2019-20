2b174d5fe47429457916507705e247026a5879fd
kgpyo <deletenickname@daum.net>
2019-12-05 18:05:38 +0900

    feat: add kakao map component
    - 카카오 맵 API에서는 react 관련 모듈을 제공하지 않으므로, useEffct
    를 통해 컴포넌트 사용시 script가 로드되도록 dom 조작
    - 키워드, 좌표변화를 객체를 복사해서 일정주기로 변화를 감지하려 하였
    으나 정상적을 동작하지 않아서 useState를 통한 상태관리 사용

---
+++ web-apps/client/src/components/kakao-map/index.js
@@ -0,0 +1,130 @@
+import React, { useEffect, useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { CircularProgress } from '@material-ui/core';
+import loadAPIScript from './load-api-script';
+import TYPE from './type';
+
+const KakaoMap = ({
+  appKey,
+  width,
+  height,
+  latitude,
+  longitude,
+  keyword,
+  callback
+}) => {
+  const [scriptLoading, setScriptLoading] = useState(false);
+  const [functions, setFunction] = useState(null);
+  const [timer, setTimer] = useState(null);
+  const kakaoMapArea = useRef(null);
+  const SEARCHDELAY = 500;
+  const actions = (containerRef) => () => {
+    // @ts-ignore
+    const { kakao } = window;
+    kakao.maps.load(() => {
+      setScriptLoading(true);
+      let marker;
+      let getAddressTimer;
+      const geocoder = new kakao.maps.services.Geocoder();
+      const container = containerRef.current;
+      const position = new kakao.maps.LatLng(33.450701, 126.570667);
+      const map = new kakao.maps.Map(container, { center: position, level: 3 });
+      map.setDraggable(true);
+
+      kakao.maps.event.addListener(map, 'center_changed', () => {
+        const center = map.getCenter();
+        const lat = center.getLat();
+        const lng = center.getLng();
+        if (marker) {
+          marker.setMap(null);
+        }
+        marker = new kakao.maps.Marker({ position: center });
+        marker.setMap(map);
+        if (getAddressTimer) {
+          clearTimeout(getAddressTimer);
+        }
+        const getAddressByMapMove = () => {
+          geocoder.coord2RegionCode(lng, lat, (result, status) => {
+            if (status !== kakao.maps.services.Status.OK) {
+              return;
+            }
+            if (callback) {
+              callback(TYPE.COORDINATE, {
+                lat,
+                lng,
+                name: result[0].address_name,
+                depth3: result[0].region_3depth_name,
+              });
+            }
+          });
+        }
+        getAddressTimer = setTimeout(getAddressByMapMove, SEARCHDELAY);
+      });
+
+      const findCoordsByAddress = (address) => {
+        geocoder.addressSearch(address, (result) => {
+          callback(TYPE.ADDRESS, result);
+        });
+      };
+      const findAddressByCoords = (lat, lng) => {
+        geocoder.coord2RegionCode(lng, lat, (result, status) => {
+          if (status !== kakao.maps.services.Status.OK) {
+            return;
+          }
+          map.setCenter(new kakao.maps.LatLng(lat, lng));
+        });
+      }
+      setFunction({
+        findCoordsByAddress,
+        findAddressByCoords
+      })
+    });
+  }
+
+  useEffect(loadAPIScript(appKey, actions(kakaoMapArea)), []);
+
+  const processQuarterlyLocation = () => {
+    if (!functions) {
+      return;
+    }
+    if (timer) {
+      clearTimeout(timer);
+      setTimer(null);
+    }
+    const newTimer = setTimeout(() => {
+      if (keyword.length) {
+        functions.findCoordsByAddress(keyword);
+      } else if (latitude && longitude) {
+        functions.findAddressByCoords(latitude, longitude);
+      }
+    }, SEARCHDELAY);
+    setTimer(newTimer);
+  };
+  useEffect(processQuarterlyLocation, [keyword, latitude, longitude]);
+
+  if (!scriptLoading) {
+    return <><CircularProgress /></>;
+  }
+
+  return (
+    <>
+      <div ref={kakaoMapArea} style={({ width, height })}>&nbsp;</div>
+    </>
+  );
+};
+
+KakaoMap.propTypes = {
+  appKey: PropTypes.string.isRequired,
+  width: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
+  keyword: PropTypes.string.isRequired,
+  latitude: PropTypes.number.isRequired,
+  longitude: PropTypes.number.isRequired,
+  callback: PropTypes.func,
+};
+
+KakaoMap.defaultProps = {
+  callback: undefined,
+};
+
+export default KakaoMap;
