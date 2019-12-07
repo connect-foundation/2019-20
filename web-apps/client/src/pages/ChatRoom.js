57e9c842db4981da46088323770d04c18769adcf
LEE SUK JAE <lsj7000@gmail.com>
2019-12-07 15:45:13 +0900

    feat: implement chat room page
    - 1:1 채팅 화면 구현.
    - 다음과 같은 특징을 갖음
    1. 새로운 채팅 메세지는 화면의 아래에서 시작
    2. 과거 오래된 메세지는 화면의 상단에서 계속해서 불러옴
    3. 과거 오래된 메세지를 불러오기 위해서 Intersection Observer를 이용하였고, 상단의 엘리먼트에 닿았을 때 추가적으로 서버로부터 과거의 채팅 메세지를 불러와서 렌더링( Intersection Observer 는 custom hooks로 제작)
    4. 과거 메세지가 추가적으로 화면의 위에 쌓여도, 스크롤을 조정하여 보고 있는 화면은 그대로 유지(새로운 엘리먼트들이 상단에 들어와도, 화면은 현재 보이는 곳에 유지한 채로, 스크롤만 조정)
    5. 상대방 혹은 사용자가 새로운 채팅 메세지 입력시에, 스크롤이 자동으로 해당 부분으로 포커싱
    6. 전체적으로 서버와 Socket.IO 연결을 통해 메세지를 주고받음

---
+++ web-apps/client/src/pages/ChatRoom.js
@@ -0,0 +1,209 @@
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useContext,
+} from 'react';
+
+// material ui
+import List from '@material-ui/core/List';
+import {makeStyles} from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+
+// components
+import ChatInputField from '../components/ChatInputField';
+import ChatSection from '../components/ChatSection';
+
+// hooks
+import useIntersect from '../hooks/useIntersect';
+
+// context
+import {ChatSocketContext} from '../contexts/socket';
+
+// utils
+import {isInSameDay, isEmptyArr, setBaseDate, formatChat} from '../utils';
+
+// fake datas
+import getConversations from '../dummy/chat';
+
+// material ui style
+const useStyles = makeStyles((theme) => ({
+  bottomFix: {
+    position: 'absolute',
+  },
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+    position: 'relative',
+
+    paddingBottom: '10rem',
+  },
+  listSection: {
+    backgroundColor: 'inherit',
+  },
+  ul: {
+    backgroundColor: 'inherit',
+    padding: 0,
+  },
+  liHeader: {
+    textAlign: 'center',
+  },
+  goToBottomBTN: {
+    position: 'fixed',
+    bottom: '3rem',
+    right: '1rem',
+  },
+}));
+const currentUser = 1;
+export default function Chat() {
+  const classes = useStyles({});
+  const {chatSocket} = useContext(ChatSocketContext);
+
+  const [formattedChat, setFormattedChat] = useState([]);
+  const [prevHeight, setPreveHeight] = useState(0);
+  const [newChat, setNewChat] = useState([]);
+  const chatInputRef = useRef(null);
+  const bottomEl = useRef(null);
+  const outerBoxRef = useRef(null);
+
+  const goToBottom = () => {
+    if (bottomEl.current) {
+      window.scrollTo({
+        behavior: 'smooth',
+        top: bottomEl.current.offsetTop,
+      });
+    }
+  };
+
+  const insertMessages = useCallback((messages) => {
+    setNewChat(formatChat(messages));
+  }, []);
+
+  useEffect(() => {
+    chatSocket.once('prevMessages', insertMessages);
+    return () => chatSocket.off('prevMessages');
+  }, [insertMessages, chatSocket]);
+
+  const insertNewMessage = useCallback(
+    (msgObject) => {
+      let messages = null;
+      const lastChat = newChat[newChat.length - 1];
+      const addMessageToLastBaseDate = (chats) => {
+        if (chats !== lastChat) return chats;
+        return {
+          ...chats,
+          messages: [...chats.messages, msgObject],
+        };
+      };
+      if (
+        isEmptyArr(newChat) ||
+        !isInSameDay(lastChat.baseDate, msgObject.timestamp)
+      ) {
+        messages = [
+          ...newChat,
+          {
+            baseDate: setBaseDate(msgObject.timestamp),
+            messages: [msgObject],
+          },
+        ];
+      } else {
+        messages = newChat.map(addMessageToLastBaseDate);
+      }
+      setNewChat(messages);
+      goToBottom();
+    },
+    [newChat],
+  );
+  useEffect(() => {
+    chatSocket.once('message', insertNewMessage);
+    return () => chatSocket.off('message');
+  }, [insertNewMessage, chatSocket]);
+
+  const onMessageSubmit = (e) => {
+    e.preventDefault();
+    if (chatInputRef.current.value === '') return;
+    chatSocket.emit('message', {
+      userId: currentUser,
+      content: chatInputRef.current.value,
+    });
+    chatInputRef.current.value = '';
+  };
+
+  const mockFetch = useCallback(
+    () =>
+      new Promise((res) => {
+        setTimeout(() => {
+          if (formattedChat[0]) {
+            res(getConversations(formattedChat[0].baseDate));
+          } else {
+            res(getConversations());
+          }
+        }, 500);
+      }),
+    [formattedChat],
+  );
+
+  const fetchAndUpdateData = useCallback(async () => {
+    let result;
+    try {
+      result = await mockFetch();
+    } catch (error) {
+      console.log(error);
+    }
+
+    const formattedResult = formatChat(result.reverse());
+    setFormattedChat([...formattedResult, ...formattedChat]);
+  }, [formattedChat, mockFetch]);
+
+  const fetchOnIntersect = useCallback(
+    async (E, O) => {
+      O.unobserve(E.target);
+      await fetchAndUpdateData();
+      O.observe(E.target);
+    },
+    [fetchAndUpdateData],
+  );
+
+  const [reloadRef] = useIntersect(fetchOnIntersect, {threshold: 1});
+
+  useEffect(() => {
+    if (outerBoxRef.current) {
+      const currentHeight = outerBoxRef.current.scrollHeight;
+
+      if (currentHeight > prevHeight) {
+        window.scrollTo(0, currentHeight - prevHeight);
+        setPreveHeight(currentHeight);
+      }
+    }
+  }, [formattedChat, prevHeight]);
+
+  return (
+    <>
+      <List className={classes.root} subheader={<li />} ref={outerBoxRef}>
+        <span ref={reloadRef} />
+
+        <ChatSection messagesByDate={formattedChat} currentUser={currentUser} />
+
+        <ChatSection messagesByDate={newChat} currentUser={currentUser} />
+
+        <IconButton
+          type='submit'
+          className={classes.goToBottomBTN}
+          onClick={goToBottom}
+          aria-label='search'
+          ref={bottomEl}
+        >
+          <ArrowDownwardIcon />
+        </IconButton>
+
+        <span ref={bottomEl} />
+      </List>
+      <ChatInputField
+        onMessageSubmit={onMessageSubmit}
+        chatInputRef={chatInputRef}
+      />
+    </>
+  );
+}
