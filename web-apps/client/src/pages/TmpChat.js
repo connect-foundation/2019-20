b967f724a3e60b872aa30ed5d31df77c6e25181a
LEE SUK JAE <lsj7000@gmail.com>
2019-12-07 16:05:15 +0900

    feat: implement temporary page to enter chat room
    - 채팅 페이지에 입장하기 위한 임시 페이지 입니다.
    - 현재 인증에 관련된 기능이 구현되 있지 않은 상황에서, 임의의 값을 통해 채팅방을 생성할 수 있는 로직을 포함하고 있습니다.
    
    기능 요약
    
    - 구매자 & 판매자 ID를 입력하고, 방에 입장하기 버튼을 누르면 다음과 같은 순서를 거칩니다.
    1) 구매자,판매자,상품ID 에대한 정보를 보내고 채팅방 ID를 생성/찾은 결과를 받아옵니다.
    2) 해당 채팅방 ID를 socket에 전달하여 chat namespace를 갖고, 채팅방ID를 room으로 갖는 방에 입장하게 됩니다.
    3) react router의 useHistory를 이용하여 채팅방 페이지로 이동합니다.
    
    - 채팅 ID를 입력하고, 방에 입장하기 버튼을 누르면 다음과 같은 순서를 거칩니다.
    1) 채팅방 ID를 socket에 전달하여 chat namespace를 갖고, 채팅방ID를 room으로 갖는 방에 입장하게 됩니다.
    2) react router의 useHistory를 이용하여 채팅방 페이지로 이동합니다.

---
+++ web-apps/client/src/pages/TmpChat.js
@@ -0,0 +1,94 @@
+import React, {useState, useContext} from 'react';
+import {useHistory} from 'react-router-dom';
+import axios from 'axios';
+
+// context
+import {ChatSocketProvider, ChatSocketContext} from '../contexts/socket';
+
+export default function Chat() {
+  const history = useHistory();
+  const {chatSocket} = useContext(ChatSocketContext);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [sellerId, setSellerId] = useState(null);
+  const [roomId, setRoomId] = useState(null);
+
+  const enterUserId = (e) => {
+    setCurrentUser(e.target.value);
+  };
+  const enterSellerId = (e) => {
+    setSellerId(e.target.value);
+  };
+  const enterRoomId = (e) => {
+    setRoomId(e.target.value);
+  };
+
+  // service
+  const enterRoom = (id) => {
+    chatSocket.emit('enterRoom', id);
+    history.push(`/chat/room/${id}`);
+  };
+
+  const getRoomId = async (options) => {
+    try {
+      const {
+        data: {_id},
+      } = await axios.post('http://10.180.171.184:5000/chat', options);
+      return _id;
+    } catch (error) {
+      return null;
+    }
+  };
+
+  const startChat = async (e) => {
+    e.preventDefault();
+    switch (e.target.value) {
+      case 'sellerid-buyerid':
+        if (currentUser && sellerId) {
+          const fetchedRoomId = await getRoomId({
+            buyerId: currentUser,
+            sellerId,
+            productId: 10,
+          });
+          enterRoom(fetchedRoomId);
+        }
+        break;
+      case 'roomid':
+        if (roomId) {
+          enterRoom(roomId);
+        }
+        break;
+      default:
+        break;
+    }
+  };
+  return (
+    <ChatSocketProvider>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          width: '90vh',
+          height: '50vh',
+          margin: 'auto',
+        }}
+      >
+        <div
+          style={{flex: 1, borderRight: '1px solid grey', textAlign: 'center'}}
+        >
+          <input onChange={enterUserId} placeholder='구매자 아이디' />
+          <input onChange={enterSellerId} placeholder='판매자 아이디' />
+          <button type='button' onClick={startChat} value='sellerid-buyerid'>
+            채팅 시작
+          </button>
+          ㅜ
+        </div>
+        <div style={{flex: 1, textAlign: 'center'}}>
+          <input onChange={enterRoomId} placeholder='채팅방 아이디' />
+          <button type='button' onClick={startChat} value='roomid'>
+            채팅 시작
+          </button>
+        </div>
+      </div>
+    </ChatSocketProvider>
+  );
+}
