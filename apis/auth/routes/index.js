30a8d06939ee36904cc3d789e2fdebaa470b2131
Johnie-Yeo <ducl7023@gmail.com>
2019-12-11 23:26:14 +0900

    refactor: allow credential request to use cors module
    - response header을 직접 작성한 것에서 cors 모듈을 사용하는 쪽으로 변경

--- apis/auth/routes/index.js
+++ apis/auth/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import cors from 'cors';
 import jwtValidator from './middlewares/jwtValidator';
 import {
   getUserInfoByJWT,
@@ -7,13 +8,13 @@ import {
   login,
 } from './controller/loginControl';
 import { addUser } from './middlewares/userManagement';
-import credentialResponse from './middlewares/authorizationCredential';
+import toAllowSetCookie from '../utils/corsOptions';
 
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('Welcome to 폴 auth server'));
-router.get('/myInfo', jwtValidator, credentialResponse, getUserInfoByJWT);
+router.get('/myInfo', jwtValidator, cors(toAllowSetCookie), getUserInfoByJWT);
 router.post('/addUser', jwtValidator, addUser, login);
-router.get('/logout', credentialResponse, logOutProcess);
+router.get('/logout', cors(toAllowSetCookie), logOutProcess);
 
 module.exports = router;
