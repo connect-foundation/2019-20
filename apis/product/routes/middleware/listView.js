5d7470d562eeba76404348a77c8c8f0da9d7ea6b
kgpyo <deletenickname@daum.net>
2019-12-16 00:16:46 +0900

    chore: add fuzziness option
    - 유사어 검색을 위해 fuzziness사용, 글자수에 따라 허용되는 편집거리
    가 달라지게 하기 위해서 auto 옵션 사용(최대 2글자)

--- apis/product/routes/middleware/listView.js
+++ apis/product/routes/middleware/listView.js
@@ -6,7 +6,7 @@ import {
 } from '../../core/string-conveter';
 
 const addFilter = (filter = [], query) => {
-  if ('range' in query || 'term' in query) {
+  if ('range' in query || 'term' in query || 'match' in query) {
     return [...filter, query];
   }
   return [...filter, { bool: query }];
@@ -110,7 +110,12 @@ const addOrderToOption = ({ query: { sort } }, res, next) => {
 const addKeywordTofilter = ({ query: { keyword } }, res, next) => {
   if (keyword) {
     const query = {
-      term: { title: keyword },
+      match: {
+        title: {
+          query: keyword,
+          fuzziness: 'auto',
+        },
+      },
     };
     res.locals.filter = addFilter(res.locals.filter, query);
   }
