78bce6f09cb0e888c70121ecc3e0c9e2f71178f6
Johnie-Yeo <ducl7023@gmail.com>
2019-12-15 17:05:48 +0900

    feat: add link to main page at entrance
    - 로그인 없이 입장 링크 추가

--- web-apps/client/src/components/skipLogInButton.jsx
+++ web-apps/client/src/components/skipLogInButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import {Link} from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,6 +14,9 @@ const useStyles = makeStyles((theme) => ({
     color: '#1db000',
     background: '#98FB98',
   },
+  link: {
+    textDecoration: 'none',
+  },
 }));
 
 const SkipLogInButton = () => {
@@ -20,9 +24,11 @@ const SkipLogInButton = () => {
 
   return (
     <div className={classes.root}>
-      <Button className={classes.skipButton} variant='contained'>
-        구경만 할래요
-      </Button>
+      <Link to='/main' className={classes.link}>
+        <Button className={classes.skipButton} variant='contained'>
+          구경만 할래요
+        </Button>
+      </Link>
     </div>
   );
 };
