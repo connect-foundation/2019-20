208b87a14e85ba8e4046d5acd743aa7ece511536
Johnie-Yeo <ducl7023@gmail.com>
2019-12-15 17:27:01 +0900

    chore: add animation at logo
    - 간단한 애니메이션을 추가하였습니다.

--- web-apps/client/src/components/logo.jsx
+++ web-apps/client/src/components/logo.jsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from '../assets/oemarket.png';
-import styled from 'styled-components';
+import Slide from '@material-ui/core/Slide';
+import {makeStyles} from '@material-ui/core/styles';
 
-const Wrapper = styled.div`
-  margin-bottom: 2rem;
-  color: '#555';
-`;
-const OEMarket = styled.img`
-  width: 5rem;
-`;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: 180,
+  },
+  logo: {
+    width: '5rem',
+    marginBottom: '2rem',
+  },
+  wrapper: {
+    width: 100 + theme.spacing(2),
+  },
+}));
 
 const Logo = () => {
+  const classes = useStyles();
+  const [checked, setChecked] = React.useState(false);
+
+  useEffect(() => {
+    setChecked(!checked);
+  }, []);
+
+  const duration = 1000;
   return (
-    <Wrapper>
-      <OEMarket src={logo} />
-    </Wrapper>
+    <div className={classes.root}>
+      <div className={classes.wrapper}>
+        <Slide
+          direction='down'
+          in={checked}
+          timeout={duration}
+          mountOnEnter
+          unmountOnExit
+        >
+          <img className={classes.logo} src={logo} alt='logo' />
+        </Slide>
+      </div>
+    </div>
   );
 };
 
