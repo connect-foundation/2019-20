560237ac84c77686d23e3735a7823de3ae8a60a6
kgpyo <deletenickname@daum.net>
2019-12-16 21:57:28 +0900

    feat: add keyword model for autocomplete
    - 자동완성을 구현할 때, '아이폰XR팝니다'를 보여주는 것이 아닌 '아'
    를 입력하면 '아이폰'만 자동완성이 되게 하기 위해서 사용자가 등록한
    제목을 anaylzer의 분석결과를 다시 keyword index로 저장하여 이를
    사용
    - 중복된 데이터 삽입을 방지하기 위해 해당 collection에서 word를
    index로 지정하여 처리
    - 사용자 정의 메소드 search는 모두 동일한 함수이고 공통으로 사용
    되기 때문에 별도의 common폴더를 생성하여 분리
    - elasticsearch와 연결하기 위해 hosts에 port정보와 함께 넣으면
    no living connection 오류가 발생하므로, hosts정보와 port 정보를분리

---
+++ apis/product/config/index.js
@@ -0,0 +1,14 @@
+export const mongoosasticSettings = {
+  hosts: process.env.ELASTICSEARCH,
+  port: process.env.ESPORT,
+  bulk: {
+    size: 100,
+    delay: 1000,
+  },
+};
+
+export const redisConnection = {
+  port: 6379,
+  host: '127.0.0.1',
+  password: 'test132@',
+};
