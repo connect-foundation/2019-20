861698b87ad871199eaaa3fcd14050c179095e10
kgpyo <deletenickname@daum.net>
2019-12-17 16:54:56 +0900

    fix: fixed 500 error when db connection failed
    - DB가 접속되지 않을 때 서버를 종료하여 api연결 시 실패 원인을 모르는
    것보다, 500 에러를 출력하여 왜 처리가 안되는지 명확히 표시하여
    클라이언트내에서 다른 처리가 가능하도록 하기 위해 변경
    - 불필요한 파일 제거
    - 뱌

--- apis/product/config/index.js
+++ apis/product/config/index.js
@@ -1,3 +1,26 @@
+import mongoose from 'mongoose';
+
+export const dbConnect = () => {
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    keepAlive: false,
+  };
+  let errorMessage;
+  mongoose.connect(`${process.env.MONGO_URL}`, options, (err) => {
+    if (err) {
+      errorMessage = err.toString();
+    }
+  });
+  return (req, res, next) => {
+    if (errorMessage) {
+      next({ status: 500, message: errorMessage });
+    } else {
+      next();
+    }
+  };
+};
+
 export const mongoosasticSettings = (process.env.NODE_ENV === 'test') ? {} : {
   hosts: process.env.ELASTICSEARCH,
   port: process.env.ESPORT,
