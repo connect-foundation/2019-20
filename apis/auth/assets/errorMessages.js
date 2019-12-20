615f9ea5632288a5fd3916864ede443e65462921
Johnie-Yeo <ducl7023@gmail.com>
2019-12-20 11:31:59 +0900

    fix: fix naver oauth to githbu oauth
    - 사정으로 인한 변경입니다.

--- apis/auth/assets/errorMessages.js
+++ apis/auth/assets/errorMessages.js
@@ -1,7 +1,7 @@
 export default {
   invalidJwtToken: 'invalid token',
   internalError: 'internal error',
-  naverError: 'naver authorization/resource server error',
+  githubError: 'github authorization/resource server error',
   ErrorWhenCheckMember: 'DB connection fail when check exist member or not',
   badRequest: 'Bad Request that use invalid document',
   validationError: 'Validation error',
--- apis/auth/routes/github.js
+++ apis/auth/routes/github.js
@@ -1,16 +1,14 @@
 import express from 'express';
-import redirectToNaverLoginForm from './controller/naverOAuth';
-import { getAccessToken, fetchUserInfo } from './middlewares/naverOAuth';
+import { getAccessToken, fetchUserInfo } from './middlewares/githubOAuth';
 import { checkExistMember } from './middlewares/userManagement';
 import { login } from './controller/loginControl';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('폴은 Naver OAuth 사용중');
+  res.send('오이마켓은  Github OAuth 사용중');
 });
 
-router.get('/login', redirectToNaverLoginForm);
 router.get('/callback', getAccessToken, fetchUserInfo, checkExistMember, login);
 
 module.exports = router;
