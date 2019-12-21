48bbc7ecb5b59d95d300496ed8c758613520b1ff
LEE SUK JAE <lsj7000@gmail.com>
2019-12-21 21:48:33 +0900

    chore: allow underscore-dangle & any proptypes & export without default
    - mongodb의 id형태를 그대로사용하기 위해 underscore-dangle 허용
    - 작업의 효율을 위해 proptypes의 strict한 제한 해제
    - 하나의 요소일 경우, default로 export해야 하는 조건 해제

--- web-apps/client/.eslintrc.js
+++ web-apps/client/.eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
     'react/prefer-stateless-function': 0,
     'react/jsx-filename-extension': 0,
     'react/jsx-one-expression-per-line': 0,
+    'no-underscore-dangle': 0,
+    'react/forbid-prop-types': 0,
+    'import/prefer-default-export': 0,
   },
   env: {
     browser: true,
