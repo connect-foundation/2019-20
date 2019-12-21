49b638f906ed7b77877b5945e1c0343ac7496001
LEE SUK JAE <lsj7000@gmail.com>
2019-12-21 18:22:09 +0900

    chore: modify proptypes as auth & product data changed

--- web-apps/client/src/types/index.js
+++ web-apps/client/src/types/index.js
@@ -9,22 +9,47 @@ import {
   node,
 } from 'prop-types';
 
-// 유저 정보가 변하면, 수정되어야 함
-export const uIdType = oneOfType([string, number]);
+/**
+ * user의 정보 형태
+ */
+export const userShape = shape({
+  id: string,
+  name: string,
+  email: string,
+  authority: string,
+  latitude: number,
+  longitude: number,
+  reputation: number,
+  numberOfRater: number,
+});
 
+/**
+ * chat message의 형태
+ */
 export const messageShape = shape({
   timestamp: string,
   content: string,
-  userId: uIdType,
+  userId: string,
 });
 
+/**
+ * chat message를 기준 날짜에 맞춰 나눈 형태
+ */
 export const messagesByDateShape = shape({
   baseDate: number,
   messaages: arrayOf(messageShape),
 });
 
-// 참고: https://stackoverflow.com/a/51127130/5755608
-export const refType = oneOfType([func, shape({current: instanceOf(Element)})]);
+/**
+ * chat room list(채팅방 목록) 에 사용되는 데이터 형태
+ */
+export const chatRoomListType = arrayOf(
+  shape({
+    messages: arrayOf(messageShape),
+    seller: userShape,
+    buyer: userShape,
+  }),
+);
 
 export const socketType = shape({
   current: shape({
@@ -33,4 +58,13 @@ export const socketType = shape({
   }),
 });
 
+/**
+ * react의 prop중 children 의 형태
+ */
 export const childrenType = oneOfType([arrayOf(node), node]).isRequired;
+
+/**
+ * react의 ref에 대한 형태
+ * 참고: https://stackoverflow.com/a/51127130/5755608
+ */
+export const refType = oneOfType([func, shape({current: instanceOf(Element)})]);
