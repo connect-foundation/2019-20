9e571d5d0fcb7f2e0162dd0b086ba6ba22fd973a
LEE SUK JAE <lsj7000@gmail.com>
2019-12-21 21:42:59 +0900

    feat: create button to enter chat room
    - auth 기능이 구현됨에 따라 채팅방에 입장하는 기능 구현
    - 로그인된 유저만 채팅 거래 기능 사용 가능

--- web-apps/client/src/components/ChatStartBtn.jsx
+++ web-apps/client/src/components/ChatStartBtn.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {useHistory} from 'react-router-dom';
+import axios from 'axios';
 import {makeStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
+import {UserContext} from '../contexts/User';
+
+import {CHAT, ROUTES} from '../assets/uris';
+
+import {isLoggedIn} from '../utils/auth';
+// types
+import {userShape, productShape} from '../types';
+
+const useStyles = makeStyles(() => ({
   chatButtonWrapper: {
     display: 'flex',
     alignItems: 'center',
@@ -15,15 +25,65 @@ const useStyles = makeStyles((theme) => ({
     fontSize: '0.8rem',
   },
 }));
-const ChatStartBtn = () => {
-  const classes = useStyles();
+
+const ChatStartBtn = ({seller, product}) => {
+  const classes = useStyles({});
+  const history = useHistory();
+  // @ts-ignore
+  const {user} = useContext(UserContext);
+
+  const getRoomId = async (data) => {
+    try {
+      const {
+        data: {_id},
+      } = await axios({
+        method: 'post',
+        url: CHAT.PATH,
+        withCredentials: true,
+        data,
+      });
+
+      return _id;
+    } catch (error) {
+      console.log('error', error);
+      return null;
+    }
+  };
+
+  const startChat = async (e) => {
+    e.preventDefault();
+    const fetchedRoomId = await getRoomId({
+      buyer: user,
+      seller,
+      product: {
+        ...product,
+        _id: product.id,
+      },
+    });
+    history.push(`${ROUTES.CHAT_ROOM}/${fetchedRoomId}`);
+  };
+
+  const buttonMessage = () =>
+    isLoggedIn(user) ? '채팅으로 거래하기' : '로그인 해주세요';
+
   return (
     <div className={classes.chatButtonWrapper}>
-      <Button variant='contained' className={classes.chatButton}>
-        채팅으로 거래하기
+      <Button
+        type='button'
+        onClick={startChat}
+        variant='contained'
+        className={classes.chatButton}
+        disabled={!isLoggedIn(user)}
+      >
+        {buttonMessage()}
       </Button>
     </div>
   );
+};
+
+ChatStartBtn.propTypes = {
+  seller: userShape.isRequired,
+  product: productShape.isRequired,
 };
 
 export default ChatStartBtn;
