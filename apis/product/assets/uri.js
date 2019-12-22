101c1926d99c840a1b974788e4ae20e2875f186b
kgpyo <deletenickname@duam.net>
2019-12-23 08:10:24 +0900

    fix: interest api
    - 관심사 업데이트시 유저정보 일치할 필요가 없는데 해서 오류가난것을
    제거

--- apis/product/assets/uri.js
+++ apis/product/assets/uri.js
@@ -1,8 +1,8 @@
 export const VERFIY_TOKEN_URI = 'https://auth.product.shop/myInfo';
 export const getSellerInfoURI = (id) => `http://auth.oemarket.shop:5000/seller/${id}`;
 export const WHITELIST = [
-  'https://oemarket.shop/',
-  'https://www.oemarket.shop/',
+  'https://oemarket.shop',
+  'https://www.oemarket.shop',
   'https://chat.oemarket.shop',
   'https://auth.oemarket.shop',
   'http://localhost:3000',
