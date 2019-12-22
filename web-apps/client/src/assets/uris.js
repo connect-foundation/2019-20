01f4f2ce20dc06cfc9ef1db032c078c4ce13ab96
LEE SUK JAE <lsj7000@gmail.com>
2019-12-23 04:31:51 +0900

    chore: stor uri as constant for reuse

--- web-apps/client/src/assets/uris.js
+++ web-apps/client/src/assets/uris.js
@@ -1,14 +1,15 @@
 // 인증
-const AUTH_API = 'https://auth.oemarket.shop';
+const AUTH_API = 'http://auth.oemarket.shop:5000'; // 고쳐야함
 export const AUTH = {
   LOGIN_STATUS_HANDLE: `${AUTH_API}/myinfo`,
   ADD_USER: `${AUTH_API}/addUser`,
   LOGOUT: `${AUTH_API}/logout`,
   githubLoginURI: `${AUTH_API}/github/login`,
+  FETCH_TOKEN_PATH: `${AUTH_API}/myToken`,
 };
 
 // 상품
-const PRODUCT_API = 'https://product.oemarket.shop';
+const PRODUCT_API = 'http://product.oemarket.shop:5000';
 export const PRODUCT = {
   IMAGE_HANDLE: `${PRODUCT_API}/products/picture`,
   PRODUCT_HANDLE: `${PRODUCT_API}/products`,
@@ -23,10 +24,11 @@ export const PRODUCT = {
 };
 
 // 채팅
-const CHAT_API = 'http://localhost:80';
+const CHAT_API = 'http://chat.oemarket.shop:80';
 export const CHAT = {
   NAME_SPACE: '/chat',
   INIT_PATH: `${CHAT_API}/chat`,
+  getChatListQuery: (id) => `${CHAT_API}/chat?uid=${id}`,
 };
 
 // client
@@ -45,5 +47,8 @@ export const ROUTES = {
   FAVORITE_LIST: `${VIEW_WITH_NVAIGATOR}/products/favorite`,
   SELL_LIST: `${VIEW_WITH_NVAIGATOR}/products/sell`,
   MYPAGE: `${VIEW_WITH_NVAIGATOR}/my-page`,
-  CHAT_ROOM: '/chat/room',
+  CHAT: `${VIEW_WITH_NVAIGATOR}/chat`,
+  CHAT_ROOM: `${VIEW_WITH_NVAIGATOR}/chat/room`,
+  CHAT_ROOM_WITH_ID: `${VIEW_WITH_NVAIGATOR}/chat/room/:_id`,
+  getChatRoomPath: (id) => `${VIEW_WITH_NVAIGATOR}/chat/room/${id}`,
 };
