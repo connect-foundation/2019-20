f3f9f12f4284e6b960dc17fd4652b0906ecba860
kgpyo <deletenickname@duam.net>
2019-12-23 04:20:36 +0900

    fix: add token validation middleware to verify users
    - 유저정보가 올바른지 확인하기 위해 토큰검증 후 진행
    - 토큰 검증을 위해 jwt 모듈 설치

--- apis/product/routes/middleware/user.js
+++ apis/product/routes/middleware/user.js
@@ -1,22 +1,13 @@
-// import axios from 'axios';
-// import { VERFIY_TOKEN_URI } from '../../assets/uri';
+import jwt from 'jsonwebtoken';
 
 const isLoggedInUser = async (req, res, next) => {
-  // try {
-  //   const response = await axios.get(VERFIY_TOKEN_URI, {
-  //     headers: { 'Set-Cookie': `jwt=${req.cookies.jwt}` }
-  //   })
-  //   if (response.status !== 200) {
-  //     next({ status: 403, message: '비정상적인 접근' })
-  //   } else {
-  //     res.locals = response.data.id;
-  //     next();
-  //   }
-  // } catch (e) {
-  //   next({ status: 500, message: e.toString() });
-  // }
-  res.locals.userId = '김철수';
-  next();
+  try {
+    const token = jwt.verify(req.cookies.jwt, process.env.TOKEN);
+    res.locals.userId = token.id;
+    next();
+  } catch (e) {
+    next({ status: 403, message: '비정상적인 접근' });
+  }
 };
 
 export default isLoggedInUser;
